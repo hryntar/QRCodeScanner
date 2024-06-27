c2cfaf03c352d7effbaf490491bd0b2288600b66
Hrynchuk Taras <hrynchuktt@gmail.com>
2024-06-27 17:25:34 +0300

    fix

--- scanner.js
+++ scanner.js
@@ -25,7 +25,7 @@ class BarcodeScanner {
                })
                   .then((response) => response.json())
                   .then((data) => {
-                     resolve(data);
+                     resolve(console.log(data));
                   })
                   .catch((error) => {
                      reject(error);
